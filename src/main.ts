67655f7f719a0cc2a268bbf05564c83de34d8788
Maksim Kirylka <maksim_kirylka@epam.com>
2022-07-16 12:11:37 +0300

    Add .env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
+
+dotenv.config({ path: __dirname + '../.env' });
+
+const port = Number(process.env.PORT) || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
