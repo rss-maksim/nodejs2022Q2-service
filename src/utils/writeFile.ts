4c59f5e432174d297b0eecb6e274c0495eacbc9c
Maksim Kirylka <maksim_kirylka@epam.com>
2022-08-06 00:27:53 +0300

    feat: Add logs file rotation in size

--- src/utils/writeFile.ts
+++ src/utils/writeFile.ts
@@ -1,10 +1,23 @@
-import { promises, existsSync, mkdirSync } from 'fs';
+import { promises, existsSync, mkdirSync, statSync } from 'fs';
+
+import env from '../env';
 
 const LOGS_DIRECTORY = 'logs';
+
+function getFilesizeInBytes(filename) {
+  const stats = statSync(filename);
+  return stats.size;
+}
 
 export const writeFile = async (filename: string, text: string) => {
   if (!existsSync(LOGS_DIRECTORY)) {
     mkdirSync(LOGS_DIRECTORY);
+  } else {
+    const size = getFilesizeInBytes(filename);
+    const maxSize = parseInt(env.MAX_LOG_FILE_SIZE, 10) || 1024;
+    if (size > maxSize) {
+      await promises.truncate(filename, 0);
+    }
   }
   await promises.writeFile(filename, text, {
     encoding: 'utf8',
